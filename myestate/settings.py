8a3d5bb2667bce8ff88cfb5ce82812dbed9893bf
Herve Tshombe <daddyhwhynot@gmail.com>
2023-09-11 22:15:38 +0200

    create DB

--- myestate/settings.py
+++ myestate/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'myestate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'realestate',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5433',
+
     }
 }
 
