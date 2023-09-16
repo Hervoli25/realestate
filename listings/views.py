52bb455d4a0af8fe8e99e203cf45891cf2aadb3f
Herve Tshombe <daddyhwhynot@gmail.com>
2023-09-16 19:36:16 +0200

    work on about and index page implementing a dynamic display of listings and realtors

--- listings/views.py
+++ listings/views.py
@@ -1,23 +1,33 @@
-from multiprocessing import context
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Listing
 
 
-# Create your views here.
-
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    # Change '3' to the desired number of items per page
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+
+    try:
+        paged_listings = paginator.page(page)
+    except PageNotAnInteger:
+        paged_listings = paginator.page(1)
+    except EmptyPage:
+        paged_listings = paginator.page(paginator.num_pages)
+
     context = {
-        'listings': listings,
+        'listings': paged_listings,
     }
-    # Passing the context to the render function
+
     return render(request, 'listings/listings.html', context)
 
 
 def listing(request, listing_id):
-
+    # Your listing view logic here
     return render(request, 'listings/listing.html')
 
 
 def search(request):
+    # Your search view logic here
     return render(request, 'listings/search.html')
