d06af5d52d595d0c986bbe15901b01a4c3048c5e
Herve Tshombe <daddyhwhynot@gmail.com>
2023-09-17 19:26:36 +0200

    create accounts app login, register and dashboard.html

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 # Create your views here.
 
@@ -12,6 +13,9 @@ def index(request):
 
     context = {
         'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
     }
 
     return render(request, 'index.html', context)
