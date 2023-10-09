cacf411c8a62c3dc32719396c3a15aeaaf7523b9
Herve Tshombe <daddyhwhynot@gmail.com>
2023-10-09 19:30:45 +0200

    removed smtp

--- myestate/settings.py
+++ myestate/settings.py
@@ -156,10 +156,3 @@ MESSAGE_TAGS = {
 }
 
 # Email config
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'hervolitshomber@gmail.com'
-EMAIL_HOST_PASSWORD = 'FoxtrotLima7#'
